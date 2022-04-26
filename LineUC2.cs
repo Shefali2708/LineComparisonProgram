67a5c2a94e688566666fe2aed3fe2a7723fb7608
shefali <shefalis2708@gmail.com>
2022-04-26 23:47:49 +0530

    Compare Two Line

---
+++ LineUC2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    class LineUC2
+    {
+        //uc2
+        public double x1, y1, x2, y2;
+        public void Values()
+        {
+            //-A Length as 2 Points(x1, y1) and(x2, y2) 
+            Console.WriteLine("Enter x1");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            y2 = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetLength()
+        {
+            //- Length of a Line = sqrt((x2 - x1) ^ 2 + (y2- y1) ^ 2)
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return length;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,11 +8,38 @@ namespace LineComparison
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
 
-            LineUC1 LineCompUC1 = new LineUC1();
-            LineCompUC1.Values();
-            double length = LineCompUC1.GetLength();
-            Console.WriteLine("Length of first Line according to points is= " + length);
-            Console.ReadLine();
+            //UC1
+            //LineUC1 LineUC1 = new LineUC1();
+            //LineUC1.Values();
+            // double length = LineUC1.GetLength();
+            //Console.WriteLine("Length of first Line according to points is= " + length);
+            //Console.ReadLine();
+
+            //UC2
+            Console.WriteLine("Welcome to Line Comparison Computation program on Master Branch");
+            LineUC2 firstLine = new LineUC2();
+            LineUC2 secondLine = new LineUC2();
+            firstLine.Values();
+            double firstLineLength = firstLine.GetLength();
+            Console.WriteLine("Length of first Line: " + firstLineLength);
+            secondLine.Values();
+            double secondLineLength = secondLine.GetLength();
+            Console.WriteLine("Length of Second Line: " + secondLineLength);
+
+            bool status = LengthComparison(firstLineLength, secondLineLength);
+            if (status)
+            {
+                Console.WriteLine("Both the Line lengths are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Line Lengths are not Equal");
+            }
+        }
+
+        public static Boolean LengthComparison(double firstLength, double secondLength)
+        {
+            return firstLength.Equals(secondLength);
         }
     }
 }
