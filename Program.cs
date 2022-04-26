4eb1bde43109c96a0cf59f6f51e108f00af0f66d
shefali <shefalis2708@gmail.com>
2022-04-26 23:32:33 +0530

    Calculate Length

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+
+            LineUC1 LineCompUC1 = new LineUC1();
+            LineCompUC1.Values();
+            double length = LineCompUC1.GetLength();
+            Console.WriteLine("Length of first Line according to points is= " + length);
+            Console.ReadLine();
+        }
+    }
+}
