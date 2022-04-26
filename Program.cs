4ba448d80990a30ab8471cc97a4a30c046b8591a
shefali <shefalis2708@gmail.com>
2022-04-27 00:07:49 +0530

    Equal,Greater,Less

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@ namespace LineComparison
             //Console.WriteLine("Length of first Line according to points is= " + length);
             //Console.ReadLine();
 
-            //UC2
+            //UC3
             Console.WriteLine("Welcome to Line Comparison Computation program on Master Branch");
-            LineUC2 firstLine = new LineUC2();
-            LineUC2 secondLine = new LineUC2();
+            LineUC3 firstLine = new LineUC3();
+            LineUC3 secondLine = new LineUC3();
             firstLine.Values();
             double firstLineLength = firstLine.GetLength();
             Console.WriteLine("Length of first Line: " + firstLineLength);
@@ -26,20 +26,26 @@ namespace LineComparison
             double secondLineLength = secondLine.GetLength();
             Console.WriteLine("Length of Second Line: " + secondLineLength);
 
-            bool status = LengthComparison(firstLineLength, secondLineLength);
-            if (status)
+            int status = LengthComparison(firstLineLength, secondLineLength);
+
+            if (status > 0)
             {
-                Console.WriteLine("Both the Line lengths are Equal");
+                Console.WriteLine("Length Of Line First is greater");
+            }
+            else if (status < 0)
+            {
+                Console.WriteLine("Length Of Line Second is greater");
             }
             else
             {
-                Console.WriteLine("Line Lengths are not Equal");
+                Console.WriteLine("Length Of both Line are Equal");
             }
+            Console.ReadLine();
         }
 
-        public static Boolean LengthComparison(double firstLength, double secondLength)
+        public static int LengthComparison(double firstLength, double secondLength)
         {
-            return firstLength.Equals(secondLength);
+            return firstLength.CompareTo(secondLength);
         }
     }
 }
